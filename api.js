8fe8ceb35f0eeba7c475d8b8fc47f2e31749760d
AshokKadubandi <velmaashok765@gmail.com>
2024-11-30 14:36:42 +0530

    backend

---
+++ api.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+const dotenv = require('dotenv')
+dotenv.config()
+const mongoose = require('mongoose')
+const {DB_USER,DB_PASSWORD} = process.env
+const PORT = process.env.PORT || 4000
+const {data} = require('./product-data')
+// console.log(data)
+
+// const productData = [{name:'Mouse',price:2599},{name:'Iphone 15',price:112805}]
+
+const dbUrl = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.cltye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.connect(dbUrl).then(function(connection){
+    console.log('connection successfull')
+})
+const productSchemaRules = {
+    name:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    imgUrl:{
+        type:String,
+        required:true
+    }
+}
+
+const userSchema = new mongoose.Schema(productSchemaRules)
+//this user userSchema will have queries or syntax
+const productModel = mongoose.model('ProductsModel',userSchema)
+
+app.post('/insert', async (req,res) => {
+    // res.send('api successfully created')
+    const ins = await productModel.insertMany(data)
+    res.status(200).json({
+        message:'Successfully Added the data'
+    })
+
+})
+
+app.get('/data',async (req,res) => {
+    const products = await productModel.find()
+    res.status(200).json({
+        message:'Successfully data retrived',
+        products,
+    })
+})
+
+app.listen(PORT,() => {
+    console.log(`https://localhost:${PORT}/`)
+})
---
+++ product-data.js
@@ -0,0 +1,5 @@
+exports.data = [
+    {name:'Iphone 15',price:115899,imgUrl:'https://m.media-amazon.com/images/I/71d7rfSl0wL.jpg'},
+    {name:'Realme 11',price:22999,imgUrl:'https://i.gadgets360cdn.com/products/large/realme-11-5g-674x800-1690807900.jpg?downsize=*:360'},
+    {name:'Samsung 4K LED',price:28999,imgUrl:'https://images.samsung.com/is/image/samsung/p6pim/in/ua75cu7700kxxl/gallery/in-crystal-uhd-cu7000-ua75cu7700kxxl-535859866?$650_519_PNG$'}
+]    
